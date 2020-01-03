7c491579466d53be1ebaa19b181840598b9d33e6
Doni Mamanazarov <doniyorad@gmail.com>
2020-01-02 20:52:44 -0600

    first commit

--- Beginner/src/com/company/Main.java
+++ Beginner/src/com/company/Main.java
@@ -3,16 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
-        int [] myArray = {1, 2 , 3, 4};
-        //System.out.println(myArray[0]);
-
-
-        String [] myStringArray = {"Hello", "Hi", "What", "Are"};
-        System.out.println(myStringArray[0]);
-        System.out.println(myStringArray.length);
-
+        int[] myArray = {1, 2, 3, 4, 5, 6, 7};
+        String[] myStringArray = {"Hello", "Hi", "What", "Are"};
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+//        System.out.println(myStringArray[0]);
+        //System.out.println(myArray.length);
 
     }
 }
