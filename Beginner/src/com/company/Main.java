6e07a80a2a3d2d84159fc4ec735ec3971daf608a
Doni Mamanazarov <doniyorad@gmail.com>
2020-01-02 22:22:17 -0600

    done

--- Beginner/src/com/company/Main.java
+++ Beginner/src/com/company/Main.java
@@ -10,10 +10,32 @@ public class Main {
 //        for (int i = 0; i < myArray.length; i++) {
 //            System.out.println(myArray[i]);}
 
-        int [] newArray = new int [5];
-        String [] newStringArray = new String [5];
+//        int [] newArray = new int [5];
+//        String [] newStringArray = new String [5];
+//
+//        newArray[0] = 3;
+//        newArray[1] = 4;
+//        newArray[2] = 2;
+//        newArray[3] = 6;
+//        newArray[4] = 9;
+//        for (int i = 0; i < newArray.length ; i++) {
+//            System.out.println(newArray[i]);
+//        }
 
-        newArray[0] 
+        String [] newStringArray = new String [6];
+
+        newStringArray[0] ="Hello";
+        newStringArray[1] ="World!";
+        newStringArray[2] =" This ";
+        newStringArray[3] ="is ";
+        newStringArray[4] ="the ";
+        newStringArray[5] ="best thing that I can do!";
+
+        for (int i = 0; i < newStringArray.length; i++) {
+            System.out.print(newStringArray[i]);
+
+        }
+
 
 //        System.out.println(myStringArray[0]);
         //System.out.println(myArray.length);
