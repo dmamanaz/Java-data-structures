0e52c2d8d69fd69336dfdabf95af1ffeec6c9cc2
Doni Mamanazarov <doniyorad@gmail.com>
2020-01-02 21:42:00 -0600

    adding arrays

--- Beginner/src/com/company/Main.java
+++ Beginner/src/com/company/Main.java
@@ -5,11 +5,16 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        int[] myArray = {1, 2, 3, 4, 5, 6, 7};
-        String[] myStringArray = {"Hello", "Hi", "What", "Are"};
-        for (int i = 0; i < myArray.length; i++) {
-            System.out.println(myArray[i]);
-        }
+//        int[] myArray = {1, 2, 3, 4, 5, 6, 7};
+//        String[] myStringArray = {"Hello", "Hi", "What", "Are"};
+//        for (int i = 0; i < myArray.length; i++) {
+//            System.out.println(myArray[i]);}
+
+        int [] newArray = new int [5];
+        String [] newStringArray = new String [5];
+
+        newArray[0]
+
 //        System.out.println(myStringArray[0]);
         //System.out.println(myArray.length);
 
