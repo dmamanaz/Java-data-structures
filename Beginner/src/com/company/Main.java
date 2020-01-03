26243707fff414ba0a3320c441601a91f0dc3dc6
Doni Mamanazarov <doniyorad@gmail.com>
2020-01-02 19:20:58 -0600

    initial commit

---
+++ Beginner/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int [] myArray = {1, 2 , 3, 4};
+        //System.out.println(myArray[0]);
+
+
+        String [] myStringArray = {"Hello", "Hi", "What", "Are"};
+        System.out.println(myStringArray[0]);
+        System.out.println(myStringArray.length);
+
+
+    }
+}
